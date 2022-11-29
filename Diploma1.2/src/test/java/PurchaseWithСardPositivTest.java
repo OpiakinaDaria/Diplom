1e2ed5a4d224512cd737ee4dcf7696bbec2ca4c6
Дарья Булдакова <daryabuldakova@MacBook-Pro-Dara.local>
2022-11-29 22:13:25 +0300

    PositiveTestes

--- Diploma1.2/src/test/java/PurchaseWithСardPositivTest.java
+++ Diploma1.2/src/test/java/PurchaseWithСardPositivTest.java
@@ -3,13 +3,10 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.time.Duration;
-
-import static com.codeborne.selenide.Condition.exist;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
-public class PurchaseWithDebitСardTest {
+public class PurchaseWithСardPositivTest {
 
     @BeforeEach
     void setUp() {
@@ -25,6 +22,17 @@ public class PurchaseWithDebitСardTest {
     @Test
     void paymentForDebitCard() {
         $$("button").find(Condition.exactText("Купить")).click();
+        $(byText("Номер карты")).parent().$(".input__control").setValue("4444 4444 4444 4441");
+        $(byText("Месяц")).parent().$(".input__control").setValue("11");
+        $(byText("Год")).parent().$(".input__control").setValue("22");
+        $(byText("Владелец")).parent().$(".input__control").setValue("Ivanov Ivan");
+        $(byText("CVC/CVV")).parent().$(".input__control").setValue("999");
+        $$("button").find(Condition.exactText("Продолжить")).click();
+        $(byText("Успешно")).parent().$(".notification");
+    }
+    @Test
+    void paymentForCredit() {
+        $$("button").find(Condition.exactText("Купить в кредит")).click();
         $(byText("Номер карты")).parent().$(".input__control").setValue("4444 4444 4444 4441");
         $(byText("Месяц")).parent().$(".input__control").setValue("11");
         $(byText("Год")).parent().$(".input__control").setValue("22");
