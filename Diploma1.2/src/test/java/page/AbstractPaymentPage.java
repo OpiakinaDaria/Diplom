329624e7079143b08c34c2a53d938c66bcc77b84
Дарья Булдакова <daryabuldakova@MacBook-Pro-Dara.local>
2022-12-11 22:55:19 +0300

    Page

---
+++ Diploma1.2/src/test/java/page/AbstractPaymentPage.java
@@ -0,0 +1,13 @@
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.ElementsCollection;
+
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.$$;
+
+public abstract class AbstractPaymentPage {
+    private ElementsCollection headings = $$("h3");
+    public AbstractPaymentPage(String title){headings.find(Condition.exactText(title)).shouldBe(visible);}
+    public FormPage goToFormPage(){
+        return new FormPage();
+    }
+}
